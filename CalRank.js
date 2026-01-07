840ff9490ae630e12a7c280ef4c31afe9ce12feb
Gustavo_25 <gustavo.h.n.godoi@gmail.com>
2026-01-07 16:27:27 +0000

    ajuste 4

--- CalRank.js
+++ CalRank.js
@@ -4,22 +4,26 @@ function calcularRank(vitorias, derrotas) {
 
     if (vitorias < 10) {
         nivel = "Ferro";
-    }else if (vitorias >= 11 && vitorias <= 20) {
+    } else if (vitorias >= 11 && vitorias <= 20) {
         nivel = "Bronze";
-    }else if (vitorias >= 21 && vitorias <= 50) {
+    } else if (vitorias >= 21 && vitorias <= 50) {
         nivel = "Prata";
-    }else if (vitorias >= 51 && vitorias <= 80) {
+    } else if (vitorias >= 51 && vitorias <= 80) {
         nivel = "Ouro";
-    }else if (vitorias >= 81 && vitorias <= 90) {
+    } else if (vitorias >= 81 && vitorias <= 90) {
         nivel = "Diamante";
-    }else if (vitorias >= 91 && vitorias <= 100) {
+    } else if (vitorias >= 91 && vitorias <= 100) {
         nivel = "Lendario";
-    }else{
+    } else {
         nivel = "Imortal";
-    } 
-    
-    return `O Herói tem de saldo de **${saldoVitorias}** vitorias está na patente **${nivel}**`;
+    }
+
+    return {
+        texto: `O Herói tem saldo de ${saldoVitorias} vitórias e está na patente ${nivel}`,
+        nivel: nivel
+    };
 }
+
 
 function mostrarRank() {
     const vitorias = Number(document.getElementById("vitorias").value);
